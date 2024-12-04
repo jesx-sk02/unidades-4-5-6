512b5e06ebc2efbb5458204f2d9a9d38502a012d
Josee <gallegos.jm02@hotmail.com>
2024-12-04 16:06:03 -0600

    Add files via upload

---
+++ Practicas visual-arduino/Nivel de lluvia/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Nivel_de_lluvia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sensor lluvia FC-37 o YL-83";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado del sensor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(364, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 136);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "comenzar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 323);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Practicas visual-arduino/Nivel de lluvia/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO.Ports;
+
+namespace Nivel_de_lluvia
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private bool isBlinking;
+        private SoundPlayer soundPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort("COMO3", 9600);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            button1.Click += button1_Click;
+            timer1.Interval = 500;
+            timer1.Tick += timer1_Tick;
+            soundPlayer = new SoundPlayer();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            this.Invoke(new Action(() =>
+            {
+                label1.Text = "Sensor Status:" + (data.Trim() == "o" ? "Water Detected" : "No Water");
+                if (data.Trim() == "o")
+                {
+                    if (!isBlinking)
+                    {
+                        timer1.Start();
+                        soundPlayer.PlayLooping();
+                        pictureBox1.BackColor = System.Drawing.Color.Red;
+                        isBlinking = true;
+                    }
+                }
+                else
+                {
+                    if (isBlinking)
+                    {
+                        soundPlayer.Stop();
+                        pictureBox1.BackColor = System.Drawing.Color.Gray;
+                        isBlinking = false;
+                    }
+                }
+            }));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = pictureBox1.BackColor == System.Drawing.Color.Gray ?
+            System.Drawing.Color.Yellow : System.Drawing.Color.Gray;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
