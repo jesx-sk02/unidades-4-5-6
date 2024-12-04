b5edbdd6dce8dbeb87089efe6569af4b4ec96cb9
Josee <gallegos.jm02@hotmail.com>
2024-12-04 16:05:19 -0600

    Add files via upload

---
+++ Practicas visual-arduino/Humedad y Temperatura/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Humedad_y_Temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.temperaturaLabel = new System.Windows.Forms.Label();
+            this.humedadLabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(29, 168);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // temperaturaLabel
+            // 
+            this.temperaturaLabel.AutoSize = true;
+            this.temperaturaLabel.Location = new System.Drawing.Point(39, 126);
+            this.temperaturaLabel.Name = "temperaturaLabel";
+            this.temperaturaLabel.Size = new System.Drawing.Size(44, 16);
+            this.temperaturaLabel.TabIndex = 4;
+            this.temperaturaLabel.Text = "label1";
+            this.temperaturaLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // humedadLabel
+            // 
+            this.humedadLabel.AutoSize = true;
+            this.humedadLabel.Location = new System.Drawing.Point(26, 236);
+            this.humedadLabel.Name = "humedadLabel";
+            this.humedadLabel.Size = new System.Drawing.Size(44, 16);
+            this.humedadLabel.TabIndex = 5;
+            this.humedadLabel.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(266, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 292);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.humedadLabel);
+            this.Controls.Add(this.temperaturaLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label temperaturaLabel;
+        private System.Windows.Forms.Label humedadLabel;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Practicas visual-arduino/Humedad y Temperatura/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Humedad_y_Temperatura
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        bool puertoCerrado = false;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.PortName = "COM3";
+            serialPort.BaudRate = 9600;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (puertoCerrado == false)
+            {
+                conectar();
+            }
+            else
+            {
+                noConectado();
+            }
+        }
+        private void conectar()
+        {
+            try
+            {
+                puertoCerrado = true;
+                serialPort.Open();
+                button1.BackColor = Color.Black;
+                button1.Text = "DESCONECTAR";
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            }
+            catch (Exception Error)
+            {
+                MessageBox.Show(Error.Message);
+            }
+        }
+        private void noConectado()
+        {
+            try
+            {
+                puertoCerrado = false;
+                serialPort.Close();
+                button1.Text = "CONECTAR";
+                button1.BackColor = Color.Plum;
+                listBox1.Items.Clear();
+                temperaturaLabel.Text = "TEMPERATURA °C";
+                humedadLabel.Text = "HUMEDAD%";
+            }
+            catch (Exception Error)
+            {
+                MessageBox.Show(Error.Message);
+            }
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            this.Invoke(new MethodInvoker(delegate
+            {
+                string[] values = data.Split('\t');
+                if (values.Length == 2)
+                {
+                    temperaturaLabel.Text = values[1];
+                    humedadLabel.Text = values[0];
+                    listBox1.Items.Add(values[1] + "  " + values[0]);
+                }
+            }));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
+
+    
+
+
